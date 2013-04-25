d0ecb6a0f6162099d96cd6071ce6af102baabe1f
huguet10@gmail.com <huguet10@gmail.com@3140f741-6ace-d83f-0de5-f38342e75178>
2013-04-25 18:22:16 +0000

    Se actualiza spring a 3.1.4 y se externaliza la url del webservice a un properties

--- apuestas/apuestas.batch/src/main/java/com/upsam/apuestas/batch/config/SpringBatchConfiguration.java
+++ apuestas/apuestas.batch/src/main/java/com/upsam/apuestas/batch/config/SpringBatchConfiguration.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -17,8 +19,19 @@ import org.springframework.web.client.RestTemplate;
  */
 @Configuration
 @ComponentScan("com.upsam.apuestas.batch")
+@PropertySource("classpath:/data.properties")
 @EnableScheduling
 public class SpringBatchConfiguration {
+
+	/**
+	 * Property sources placeholder configurer.
+	 * 
+	 * @return the property sources placeholder configurer
+	 */
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 
 	/**
 	 * /** Rest template.
